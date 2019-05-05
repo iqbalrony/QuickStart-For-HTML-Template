0c1a14fea2714d1cbbcf50361b573817921147e8
Iqbal Rony <iqbalrony30@gmail.com>
2019-05-05 13:04:48 +0600

    v.01

---
+++ Without-Bootstrap/js/active.js
@@ -0,0 +1,33 @@
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function ($) {
+
+		$(".embed-responsive iframe").addClass("embed-responsive-item");
+		$(".carousel-inner .item:first-child").addClass("active");
+
+		$('[data-toggle="tooltip"]').tooltip();
+
+		//    Remove Active Class from Menu Item
+		$(".works_menu ul li span").click(function () {
+			$(".works_menu ul li span").removeClass("activate");
+		});
+
+		//    Add Active Class to Menu Item
+		$(".works_menu ul li span").click(function () {
+			$(this).addClass("activate");
+		});
+
+	});//document ready End
+
+
+	jQuery(window).load(function () {
+
+	});//window load End
+
+	jQuery(window).resize(function () {
+
+	});//window resize End
+
+
+}(jQuery));
